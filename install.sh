c4a8b91b1c9f0cc1f6d0e120381a8162ab3d667e
Scott Walter <scott.walter@nice.com>
2021-05-24 14:26:37 -0500

    cleanup install process

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 npm install
 echo "shared-angular-components"; cd shared-angular-components && npm install && npm run build-lib && cd ..
-echo "preparing app1-angular12"; cd app1-angular12 && npm install && cd ..
-echo "preparing app2-angular12"; cd app1-angular12 && npm install && cd ..
+echo "preparing app1"; cd app1-angular-12.0.0 && npm install && cd ..
+echo "preparing app2"; cd app2-angular-11.0.0-rc.0 && yarn install && cd ..
+echo "preparing app3"; cd app3-angular-11.0.0-rc.0 && yarn install && cd ..
 echo "preparing container-module-federation"; cd container-module-federation && npm install && cd ..
