0d3273d3d333b9a46bc4da17a98ece50d9834eb0
Scott Walter <scott.walter@nice.com>
2021-05-25 08:48:53 -0500

    added trivial react app

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ echo "shared-angular-components"; cd shared-angular-components && npm install &&
 echo "preparing app1"; cd app1-angular-12.0.0 && npm install && cd ..
 echo "preparing app2"; cd app2-angular-11.0.0-rc.0 && yarn install && cd ..
 echo "preparing app3"; cd app3-angular-11.0.0-rc.0 && yarn install && cd ..
+echo "preparing app4"; cd app4-react-17-.0.1 && npm install && cd ..
 echo "preparing container-module-federation"; cd container-module-federation && npm install && cd ..
